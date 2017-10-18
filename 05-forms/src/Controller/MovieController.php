c1ff0042ab7811301b7632b48ece72373b05e521
Nacho Martín <nacho@limenius.com>
2017-10-18 21:32:15 +0200

    Forms starting point

---
+++ 05-forms/src/Controller/MovieController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Movie;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MovieController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        $movies = $this->getDoctrine()
+            ->getRepository(Movie::class)
+            ->findAll();
+
+        return $this->render('movies/index.html.twig', [
+            'movies' => $movies,
+        ]);
+    }
+
+    /**
+     * @Route("/movie/{movieId}", name="movie")
+     */
+    public function movie(int $movieId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        return $this->render('movies/movie.html.twig', [
+            'movie' => $em->getRepository(Movie::class)->findOneWithActors($movieId),
+        ]);
+    }
+}
